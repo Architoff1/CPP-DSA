ec56f54eab7c255258f60abb2564e4498e173b19
Architoff1 <dwivedi.archit.official@gmail.com>
2026-03-20 19:53:15 +0530

    DAY01(not completed)

---
+++ DAY2/char2num.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int char1='a'+2;
+    char char2='b'+2;
+
+    cout<<"Integer stored character: "<<char1<<endl<<"Character stored character: "<<char2<<endl;
+}
---
+++ DAY2/inpput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter your I number: ";
+    cin>>num1;
+    cout<<"Enter your II number: ";
+    cin>>num2;
+
+    cout<<"I num: "<<num1<<endl<<"II num: "<<num2<<endl;
+
+    return 0;
+}
