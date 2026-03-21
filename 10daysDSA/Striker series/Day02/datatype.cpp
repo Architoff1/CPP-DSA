1c885ae9d163edf16b47eb18d6c9485361f82e10
Architoff1 <dwivedi.archit.official@gmail.com>
2026-03-22 00:54:21 +0530

    Day02 in progress

---
+++ 10daysDSA/Striker series/Day02/datatype.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1;
+    char char1;
+    float float1;
+    bool bool1;
+    string str1;
+    cout<<"Enter your I num: ";
+    cin>>num1;
+    cout<<"\nEnter your char: ";
+    cin>>char1;
+    cout<<endl<<"Enter your II num: ";
+    cin>>float1;
+    cout<<endl<<"Enter your I bool: ";
+    cin>>bool1;
+    cout<<"\nEnter your I string: ";
+    cin>>str1;
+
+    cout<<endl<<"Outputs:\nNumber 1: "<<num1<<"\nCharacter: "<<char1<<"\nNumber 2: "<<float1<<endl;
+    cout<<"Bool: "<<bool1<<"\nString: "<<str1<<endl;
+    return 0;
+}
---
+++ 10daysDSA/Striker series/Day02/prepost.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    bool bool1;
+    cout<<"Enter your bool: ";
+    cin>>bool1;
+    bool1 = bool1+1;
+    cout<<"Pre increment: "<<bool1;
+    return 0;
+}
